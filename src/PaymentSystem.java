af02aa9d55f12bda2e4dd9a70b2d689895b6bd4b
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2023-12-06 12:26:55 +0700

    Choose Ticket and payment

---
+++ src/PaymentSystem.java
@@ -0,0 +1,21 @@
+class PaymentSystem {
+    double priceTicketVIP = 300.0;
+    double priceTicketReguler = 200.0;
+    double priceTicketBiasa = 80.0;
+    double totalprice = 0.0;
+
+    public double totalPrice(int CategoryTicket, int NumberofTicket) {
+        if (CategoryTicket == 1) {
+            totalprice = priceTicketVIP * NumberofTicket;
+        } else if (CategoryTicket == 2) {
+            totalprice = priceTicketReguler * NumberofTicket;
+        } else if (CategoryTicket == 3) {
+            totalprice = priceTicketBiasa * NumberofTicket;
+        } else {
+            totalprice = 0.0;
+            System.out.println("the ticket number you entered is invalid.");
+        }
+
+        return totalprice;
+    }
+}
